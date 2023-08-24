21b9d2d6c916b1c1f02cb0483fabb11a128f3c69
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-08-24 18:36:46 +0900

    TikTok Clone: TAB NAVIGATION-Stateful Navigation Part2(Offstage, Stack)

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -14,44 +14,6 @@ class MainNavigationScreen extends StatefulWidget {
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
   int _selectedIndex = 0;
 
-  // List of widgets 생성
-  final screens = [
-    // const Center(
-    //   child: Text(
-    //     "Home",
-    //     style: TextStyle(fontSize: Sizes.size48),
-    //   ),
-    // ),
-    // const Center(
-    //   child: Text(
-    //     "Discover",
-    //     style: TextStyle(fontSize: Sizes.size48),
-    //   ),
-    // ),
-    // Container(),
-    // const Center(
-    //   child: Text(
-    //     "Inbox",
-    //     style: TextStyle(fontSize: Sizes.size48),
-    //   ),
-    // ),
-    // const Center(
-    //   child: Text(
-    //     "Profile",
-    //     style: TextStyle(fontSize: Sizes.size48),
-    //   ),
-    // ),
-    // StfScreen처럼 여러개의 동일한 위젯을 사용 시 하나의 위젯으로 착각을 함, key를 이용해서 각각의 위젯을 유니크하게 만들어줌.
-    // 탭을 전환했다가 이전에 사용했던 탭으로 돌아가면 이전의 사용 기록이 모두 삭제되고 다시 화면이 빌드되는 문제점을 가지고 있음
-    // 이전의 사용 기록을 그대로 유지하고 싶을 때에는 탭 전환 시 dispose되는 것이 문제가 됨.
-    // 즉, 개발자가 어떤 목적(유지 또는 폐기)을 가졌는지에 따라 문제가 될 수도 안 될 수도 있음.
-    StfScreen(key: GlobalKey()),
-    StfScreen(key: GlobalKey()),
-    Container(),
-    StfScreen(key: GlobalKey()),
-    StfScreen(key: GlobalKey()),
-  ];
-
   void _onTap(int index) {
     setState(() {
       _selectedIndex = index;
@@ -61,7 +23,26 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: screens.elementAt(_selectedIndex),
+      body: Stack(
+        children: [
+          Offstage(
+            offstage: _selectedIndex != 0,
+            child: const StfScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 1,
+            child: const StfScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 3,
+            child: const StfScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 4,
+            child: const StfScreen(),
+          ),
+        ],
+      ),
       bottomNavigationBar: BottomAppBar(
         color: Colors.black,
         child: Padding(
