9ae2be1ff4b5214aa66f6a12263c788cf1b6d999
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-09-04 20:42:30 +0900

    TikTok Clone: DISCOVER-TabBar and TabBarView(collection for)

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,13 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+final tabs = [
+  "Top",
+  "Users",
+  "Videos",
+  "Sounds",
+  "LIVE",
+  "Shopping",
+  "Brands",
+];
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("discover"),
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0.3,
+          title: const Text("discover"),
+          // TabBar를 컨트롤러 없이 사용하면 에러 발생함. 가장 쉬운 해결 방법은 DefaultTabController 위젯으로 감싸줌.
+          bottom: TabBar(
+              splashFactory: NoSplash.splashFactory,
+              padding: const EdgeInsets.symmetric(
+                horizontal: Sizes.size16,
+              ),
+              isScrollable: true,
+              labelStyle: const TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: Sizes.size16,
+              ),
+              indicatorColor: Colors.black,
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.grey.shade500,
+              tabs: [
+                for (var tab in tabs)
+                  Tab(
+                    text: tab,
+                  )
+              ]),
+        ),
+        body: TabBarView(
+          children: [
+            for (var tab in tabs)
+              Center(
+                child: Text(
+                  tab,
+                  style: const TextStyle(
+                    fontSize: Sizes.size28,
+                  ),
+                ),
+              )
+          ],
+        ),
       ),
     );
   }
