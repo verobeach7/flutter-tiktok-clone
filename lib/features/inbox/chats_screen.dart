adf252326154254e7d2d66fa954d99a8ef61cbc6
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-09-16 20:47:09 +0900

    TikTok Clone: Chats-AnimatedList(Delete Item)

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/inbox/chat_detail_screen.dart';
 
 class ChatsScreen extends StatefulWidget {
   const ChatsScreen({super.key});
@@ -16,6 +17,8 @@ class _ChatsScreenState extends State<ChatsScreen> {
   final ScrollController _scrollController = ScrollController();
 
   final List<int> _items = [];
+
+  final Duration _duration = const Duration(milliseconds: 500);
 
   @override
   void initState() {
@@ -33,12 +36,70 @@ class _ChatsScreenState extends State<ChatsScreen> {
     if (_key.currentState != null) {
       _key.currentState!.insertItem(
         _items.length,
-        duration: const Duration(
-          milliseconds: 500,
-        ),
+        duration: _duration,
       );
       _items.add(_items.length);
     }
+  }
+
+  void _deleteItem(int index) {
+    if (_key.currentState != null) {
+      _key.currentState!.removeItem(
+        index,
+        (context, animation) => SizeTransition(
+          sizeFactor: animation,
+          child: Container(
+            color: Colors.red,
+            child: _makeTile(index),
+          ),
+        ),
+        duration: _duration,
+      );
+      _items.removeAt(index);
+    }
+  }
+
+  void _onChatTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const ChatDetailScreen(),
+      ),
+    );
+  }
+
+  Widget _makeTile(int index) {
+    return ListTile(
+      onLongPress: () => _deleteItem(index),
+      onTap: _onChatTap,
+      leading: const CircleAvatar(
+        radius: 30,
+        foregroundImage: NetworkImage(
+            "https://avatars.githubusercontent.com/u/60215757?v=4"),
+        child: Text(
+          "희성",
+        ),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            "혜미 ($index)",
+            style: const TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text(
+            "2:16 PM",
+            style: TextStyle(
+              color: Colors.grey.shade500,
+              fontSize: Sizes.size12,
+            ),
+          ),
+        ],
+      ),
+      subtitle: const Text("Don't forget to make video"),
+    );
   }
 
   @override
@@ -71,36 +132,7 @@ class _ChatsScreenState extends State<ChatsScreen> {
               opacity: animation,
               child: SizeTransition(
                 sizeFactor: animation,
-                child: ListTile(
-                  leading: const CircleAvatar(
-                    radius: 30,
-                    foregroundImage: NetworkImage(
-                        "https://avatars.githubusercontent.com/u/60215757?v=4"),
-                    child: Text(
-                      "희성",
-                    ),
-                  ),
-                  title: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        "혜미 ($index)",
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      Text(
-                        "2:16 PM",
-                        style: TextStyle(
-                          color: Colors.grey.shade500,
-                          fontSize: Sizes.size12,
-                        ),
-                      ),
-                    ],
-                  ),
-                  subtitle: const Text("Don't forget to make video"),
-                ),
+                child: _makeTile(index),
               ),
             );
           },
