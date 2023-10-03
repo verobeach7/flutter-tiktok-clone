f8ba06a3a1d62d5d301c770642cb26b5d8a432c3
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-10-03 21:03:47 +0900

    TikTok Clone: SETTINGS-AboutListTile(How to show about Licenses)

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SettingsScreen extends StatelessWidget {
@@ -10,45 +9,27 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Settings"),
       ),
-      /* body: const Column(
+      body: ListView(
         children: [
-          CloseButton(), // 별도의 아이콘 없이도 X 버튼 위젯을 사용할 수 있음
-        ],
-      ), */
-      /* body: ListWheelScrollView(
-        // 휠로 이루어진 아이템 리스트뷰를 사용할 수 있음
-        itemExtent: 200, // 아이템의 높이
-        /* // 확대 기능
-        useMagnifier: true,
-        magnification: 1.5, */
-        // diameterRatio: 3, // 휠이 얼마나 휘어져있는지 설정, 100으로 설정하면 평면이 됨
-        offAxisFraction: 2, // 옆으로 휘어지게 할 수 있음
-        children: [
-          for (var x in [1, 2, 1, 1, 1, 1, 1, 1, 1])
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: Container(
-                color: Colors.teal,
-                alignment: Alignment.center,
-                child: const Text(
-                  "Pick me",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: Sizes.size36,
-                  ),
-                ),
+          // Licenses를 보여주는 방법 1.
+          ListTile(
+            // showAboutDialog는 구현이 이미 되어있는 함수
+            onTap: () => showAboutDialog(
+                context: context,
+                applicationVersion: "1.0",
+                applicationLegalese:
+                    "All rights reserved. Please don't copy me."),
+            title: const Text(
+              "About",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
               ),
             ),
-        ],
-      ), */
-      body: const Column(
-        children: [
-          CupertinoActivityIndicator(
-            radius: 30,
+            subtitle: const Text("About this app......"),
           ),
-          CircularProgressIndicator(),
-          // 플랫폼(ios, android 등)에 따라 다르게 보여줌
-          CircularProgressIndicator.adaptive(),
+          // Licenses를 보여주는 방법 2.
+          // onTap: () => showAboutDialog에서 해줘야 하는 작업을 자동으로 해줌
+          const AboutListTile(),
         ],
       ),
     );
