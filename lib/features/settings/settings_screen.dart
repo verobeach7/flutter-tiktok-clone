c0a4886c336da8c1af18cb1d05e3345a7cd9f2d2
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-10-09 18:26:58 +0900

    TikTok Clone: SETTINGS-SwitchListTile, CheckboxListTile

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,7 +1,22 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _notifications = false;
+
+  void _onNotificationChanged(bool? newValue) {
+    if (newValue == null) return;
+    setState(() {
+      _notifications = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +26,43 @@ class SettingsScreen extends StatelessWidget {
       ),
       body: ListView(
         children: [
+          CupertinoSwitch(
+            value: _notifications,
+            onChanged: _onNotificationChanged,
+          ),
+          Switch(
+            value: _notifications,
+            onChanged: _onNotificationChanged,
+          ),
+          // iOS와 android를 구분하여 보여줌(Cupertino vs Material), 적응형 위젯
+          Switch.adaptive(
+              value: _notifications, onChanged: _onNotificationChanged),
+          SwitchListTile(
+            value: _notifications,
+            onChanged: _onNotificationChanged,
+            title: const Text(
+              "Enable notifications",
+            ),
+          ),
+          // SwitchListTile 사용을 추천!!!
+          SwitchListTile.adaptive(
+            value: _notifications,
+            onChanged: _onNotificationChanged,
+            title: const Text(
+              "Enable notifications",
+            ),
+            subtitle: const Text("Enable notifications"),
+          ),
+          Checkbox(
+            value: _notifications,
+            onChanged: _onNotificationChanged,
+          ),
+          CheckboxListTile(
+            activeColor: Colors.black,
+            value: _notifications,
+            onChanged: _onNotificationChanged,
+            title: const Text("Enable notifications"),
+          ),
           ListTile(
             onTap: () async {
               final date = await showDatePicker(
@@ -36,10 +88,11 @@ class SettingsScreen extends StatelessWidget {
                 builder: (context, child) {
                   return Theme(
                       data: ThemeData(
-                          appBarTheme: const AppBarTheme(
-                        foregroundColor: Colors.white,
-                        backgroundColor: Colors.black,
-                      )),
+                        appBarTheme: const AppBarTheme(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.black,
+                        ),
+                      ),
                       child: child!);
                 },
               );
